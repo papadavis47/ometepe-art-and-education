ea21d3ea60ac61ae9a21ba9d99445d0b06dfb379
John William Davis <jwdavisdeveloper@gmail.com>
2022-02-22 13:44:23 -0800

    Begin work on programs page

--- pages/info/programs.js
+++ pages/info/programs.js
@@ -1,5 +1,22 @@
+import Head from "next/head";
+
 const Programs = () => {
-  return <div>This is the Programs page.</div>;
+  return (
+    <div className='has-text-light has-text-weight-light'>
+      <Head>
+        <title>Programs</title>
+        <meta name='description' content='Ometepe Art and Education' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1 className='is-size-1 is-size-3-mobile  title mt-6 has-text-light has-text-weight-light has-text-centered'>
+        Programs
+      </h1>
+      <p className='is-size-6-mobile has-text-justified is-size-4-desktop'>
+        Our classes and workshops are designed to enrich the students lives by exposing them to
+        Latino culture, language and creativity &hearts;
+      </p>
+    </div>
+  );
 };
 
 export default Programs;
