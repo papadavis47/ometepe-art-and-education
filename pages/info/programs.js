9088b39291a84c7406fd223f2b2d930e1bb48f61
John William Davis <jwdavisdeveloper@gmail.com>
2022-02-22 19:25:32 -0800

    Work with Programs page - add image - upgrade Next to12.1

--- pages/info/programs.js
+++ pages/info/programs.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
+import Image from "next/image";
 
 const Programs = () => {
   return (
-    <div className='has-text-light has-text-weight-light'>
+    <div className='has-text-light has-text-weight-light columns is-flex-direction-column is-align-items-center'>
       <Head>
         <title>Programs</title>
         <meta name='description' content='Ometepe Art and Education' />
@@ -15,8 +16,8 @@ const Programs = () => {
         Our classes and workshops are designed to enrich the students lives by exposing them to
         Latino culture, language and creativity &hearts;
       </p>
-      <section className='block mt-4'>
-        <ul>
+      <section className='block mt-4 column'>
+        <ul className='m-auto'>
           <li className='is-size-4 is-size-6-mobile'>
             <p>Arts and Crafts - Saturdays 10am to 1pm</p>
           </li>
@@ -31,6 +32,14 @@ const Programs = () => {
           </li>
         </ul>
       </section>
+      <div className='columns is-flex is-centered mt-6'>
+        <figure className='image'>
+          <Image src='/dancers2-ometepe.jpg' width={600} height={500} alt='Dancers in Ometepe' />
+          <figcaption className='has-text-light is-size-6'>
+            Dancers, The Island of Ometepe June 2021
+          </figcaption>
+        </figure>
+      </div>
     </div>
   );
 };
