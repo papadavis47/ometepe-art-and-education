00618e41335dbd081dc165ad66a0f5372571d246
John William Davis <jwdavisdeveloper@gmail.com>
2022-04-22 10:55:16 -0700

    Fix issue with responsiveness on contact page

--- pages/info/contact.js
+++ pages/info/contact.js
@@ -4,10 +4,13 @@ const Contact = () => {
   const { spanish } = useAppContext();
   return (
     <div className='has-text-light has-textweight-light has-text-centered mt-6'>
-      <h2 className='is-size-3'>
+      <h2 className='is-size-3 is-size-5-mobile'>
         {spanish ? "Correo electrónico" : "Email"}:&nbsp;{" "}
-        <span className='has-text-weight-bold'> ometepeartandeducation@gmail.com</span>
       </h2>
+      <span className='has-text-weight-bold is-size-5-mobile is-size-3'>
+        {" "}
+        ometepeartandeducation@gmail.com
+      </span>
     </div>
   );
 };
