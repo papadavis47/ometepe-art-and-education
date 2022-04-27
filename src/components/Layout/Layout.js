aae71ed2160500e7591cc0f2b03c8a866489d451
John William Davis <jwdavisdeveloper@gmail.com>
2022-04-27 10:41:39 -0700

    Fix issue with top margin on Layout main container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className='is-flex is-flex-direction-column is-fullheight-100vh'>
       <NavBar />
-      <main className='column ml-6 mr-6'>{children}</main>
+      <main className='column ml-6 mr-6 mt-6'>{children}</main>
       <Footer />
     </div>
   );
