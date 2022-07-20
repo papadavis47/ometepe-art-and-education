09599039f5684810bc319cc87ba5307b3876a631
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-20 13:59:21 -0700

    Continue styling bioparagraph component

--- components/BioParagraph/BioParagraph.js
+++ components/BioParagraph/BioParagraph.js
@@ -1,9 +1,9 @@
 // Add context here - when I have the spanish text
 
-function BioParagraph({ text }) {
+function BioParagraph({ text, id }) {
   return (
-    <div className='my-3'>
-      <p className='text-lg sm:text-xl'>{text}</p>
+    <div className='my-3' key={id}>
+      <p className='font-serif text-xl leading-relaxed md:leading-10 sm:text-2xl'>{text}</p>
     </div>
   );
 }
