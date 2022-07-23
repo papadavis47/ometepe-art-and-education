eb56e3fc42339775c77afa030816692c03a7565e
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-23 16:14:48 -0700

    Change duration of easout on mobile menu transition

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -208,7 +208,7 @@ const NavBar = () => {
               enter='transition duration-200 ease-out'
               enterFrom='transform scale-95 opacity-0'
               enterTo='transform scale-100 opacity-100'
-              leave='transition duration-150 ease-out'
+              leave='transition duration-100 ease-out'
               leaveFrom='transform scale-100 opacity-100'
               leaveTo='transform scale-95 opacity-0'
             >
