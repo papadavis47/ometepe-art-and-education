bb98d4d90e09676edc4213925d1ab6f4f3901c14
John William Davis <jwdavisdeveloper@gmail.com>
2022-10-04 17:12:27 -0700

    Change arbitrary bg color value to hex value and decrease lightness for accesibility reasons

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,7 +3,8 @@ import Footer from "../Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className='flex flex-col min-h-screen bg-[#d26914]'>
+    // Used an arbitrary HSL value here for the bg color
+    <div className='flex flex-col min-h-screen bg-[hsl(27_83%_39%)]'>
       <NavBar />
       <main className='flex-1'>{children}</main>
       <Footer />
