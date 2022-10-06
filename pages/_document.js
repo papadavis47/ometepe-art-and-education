aae5edfc88632106001e6a4af5d1f6bea4efa96c
John William Davis <jwdavisdeveloper@gmail.com>
2022-10-06 08:49:30 -0700

    Optimize Raleway variable font from Google

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,7 @@ class MyDocument extends Document {
             rel='stylesheet'
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;1,300;1,400&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@100..900&display=swap'
             rel='stylesheet'
           />
         </Head>
