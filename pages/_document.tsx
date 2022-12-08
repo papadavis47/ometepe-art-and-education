14c4754ed5d39b534bdb5321095bc8f5a5745094
John William Davis <jwdavisdeveloper@gmail.com>
2022-12-08 10:43:04 -0800

    update syntax for raleway font in custom document, change to same as arapey

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,7 +37,7 @@ class MyDocument extends Document {
             rel='stylesheet'
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@100..900&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Raleway:ital@0,wght@100;200;300;400;500;600;700;800;900&display=swap'
             rel='stylesheet'
           />
         </Head>
