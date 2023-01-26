77e7f5c063401db86799dd1f5b60cbf88cbc31d6
John William Davis <jwdavisdeveloper@gmail.com>
2023-01-25 23:58:45 -0800

    Tweak a type definition in construction.tsx

--- components/Construction/Construction.tsx
+++ components/Construction/Construction.tsx
@@ -1,15 +1,15 @@
 import { useAppContext } from '../../Context/appContext';
 
-type props = {
+type pageName = {
   title?: string;
 };
 
-const Construction = (props) => {
+const Construction = ({ title }: pageName) => {
   const { spanish } = useAppContext();
   return (
     <>
       <div className='text-center'>
-        <h1 className='mt-6 text-4xl text-white'>{props.title}</h1>
+        <h1 className='mt-6 text-4xl text-white'>{title}</h1>
         <h2 className='text-2xl italic text-white'>
           {spanish ? 'En Proceso' : 'Under Construction'} 🔨
         </h2>
