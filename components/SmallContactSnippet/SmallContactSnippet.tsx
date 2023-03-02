26059ddd90b874d5a35aecc7b763c9e7b9fb4510
John William Davis <jwdavisdeveloper@gmail.com>
2023-03-01 22:53:51 -0800

    comment out the link from smallcontactsnippet component - will refactor later

--- components/SmallContactSnippet/SmallContactSnippet.tsx
+++ components/SmallContactSnippet/SmallContactSnippet.tsx
@@ -4,13 +4,13 @@ function SmallContactSnippet() {
   const { spanish } = useSpanishContext();
   return (
     <div className='mt-6 text-lg sm:text-xl'>
-      <h2 className=''>
+      {/* <h2 className=''>
         {spanish ? 'Para nuestro formulario de interés haga clic ' : 'For our interest form click '}
         <a className='font-bold underline' href='https://form.jotform.com/221110567022137'>
           {spanish ? 'aqui' : 'here'}
         </a>{' '}
         {spanish ? 'o:' : 'or:'}
-      </h2>
+      </h2> */}
       <p className='mt-4'>
         <span className='italic'>
           <a className='' href='mailto:ometepeartandeducation@gmail.com'>
