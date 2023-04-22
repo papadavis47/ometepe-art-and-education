b819ed4e12e270a7ce5125a0befa24588f5fc911
John William Davis <jwdavisdeveloper@gmail.com>
2023-04-22 01:17:15 -0700

    change wrapper div in layout to min-h-full from min-h-screen - more inline with Comeau suggestion due to mobile disparity

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,10 @@
-import NavBar from "../NavBar";
-import Footer from "../Footer";
+import NavBar from '../NavBar';
+import Footer from '../Footer';
 
 const Layout = ({ children }) => {
   return (
     // Used an arbitrary HSL value here for the bg color
-    <div className='flex flex-col min-h-screen bg-[hsl(27_83%_39%)]'>
+    <div className='flex flex-col min-h-full bg-[hsl(27_83%_39%)]'>
       <NavBar />
       <main className='flex-1'>{children}</main>
       <Footer />
