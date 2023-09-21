3d54266f80473b6e3518d6a3b22bb6d629ec9ad3
John William Davis <jwdavisdeveloper@gmail.com>
2023-09-20 22:19:55 -0700

    add 2 pics to contact, upgrade next to 13.5

--- components/SmallContactSnippet/SmallContactSnippet.tsx
+++ components/SmallContactSnippet/SmallContactSnippet.tsx
@@ -3,7 +3,7 @@ import { useSpanishContext } from '@context/spanishContext';
 function SmallContactSnippet() {
   const { spanish } = useSpanishContext();
   return (
-    <div className='mt-6 text-lg sm:text-xl'>
+    <div className='mt-6 text-xl sm:text-2xl'>
       {/* <h2 className=''>
         {spanish ? 'Para nuestro formulario de interés haga clic ' : 'For our interest form click '}
         <a className='font-bold underline' href='https://form.jotform.com/221110567022137'>
@@ -11,8 +11,8 @@ function SmallContactSnippet() {
         </a>{' '}
         {spanish ? 'o:' : 'or:'}
       </h2> */}
-      <p className='mt-4'>
-        <span className='italic'>
+      <p className='pb-4 mt-4'>
+        <span className='italic hover:text-yellow-300'>
           <a className='' href='mailto:ometepeartandeducation@gmail.com'>
             ometepeartandeducation@gmail.com
           </a>
--- pages/info/contact.tsx
+++ pages/info/contact.tsx
@@ -1,9 +1,32 @@
 import SmallContactSnippet from '@components/SmallContactSnippet';
+import Image from 'next/image';
 
 const Contact = () => {
   return (
-    <div className='flex justify-center px-4 text-white align-center pt-28'>
-      <SmallContactSnippet />
+    <div className='px-4 pt-16 text-white'>
+      <div className='max-w-4xl mx-auto'>
+        <div className='text-center sm:text-left'>
+          <SmallContactSnippet />
+        </div>
+        <figure className='mt-6'>
+          <Image
+            width={800}
+            height={600}
+            src='/images/landing/linda.jpg'
+            alt='Picture little girl in traditional Nicaraguan dress'
+            className='rounded-md'
+          />
+        </figure>
+        <figure className='mt-6'>
+          <Image
+            width={800}
+            height={600}
+            src='/images/landing/dollsgrey.jpg'
+            alt='Artisan holding traditional nicaraguan dolls'
+            className='rounded-md'
+          />
+        </figure>
+      </div>
     </div>
   );
 };
